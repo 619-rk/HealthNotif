0483bbefe0eda318e1bd8947b3f1e06c40bbc5dd
ravneet <rkumar2_be15@thapar.edu>
2022-02-22 23:13:14 +0530

    Initial commit

---
+++ src/main/java/com/example/HealthNotif/HealthNotifApplication.java
@@ -0,0 +1,13 @@
+package com.example.HealthNotif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthNotifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthNotifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HealthNotif/HealthNotifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HealthNotif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthNotifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
